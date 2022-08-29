b9e69f24b6243a2eaced8f13f10204bdf80769c4
Lexicon Student 1 <Lexicon Student 1@DESKTOP-OF8V11H>
2022-08-29 11:45:28 +0200

    Changes to register

--- Uppgift 14/Migrations/20220829093941_application_user+.Designer.cs
+++ Uppgift 14/Migrations/20220829093941_application_user+.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Uppgift_14.Data;
 
@@ -11,9 +12,10 @@ using Uppgift_14.Data;
 namespace Uppgift_14.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220829093941_application_user+")]
+    partial class application_user
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Uppgift 14/Models/ApplicationUser.cs
+++ Uppgift 14/Models/ApplicationUser.cs
@@ -4,7 +4,16 @@ namespace Uppgift_14.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string FullName => @"{FirstName} {LastName}";
+        public DateTime TimeOfRegistration { get; private set; }
+
         // navigation property, using relational class ApplicationUserGymClass
         public List<ApplicationUserGymClass> AttendedClasses { get; set; } = new List<ApplicationUserGymClass>(); // viktigt!
+
+        public ApplicationUser() {
+            TimeOfRegistration = DateTime.Now;
+        }
     }
 }
